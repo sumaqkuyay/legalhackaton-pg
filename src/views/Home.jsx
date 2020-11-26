bc56da92670853070001e70b2741dec46b4b8dc5
IsabelaSanchez <natyst_26@hotmail.com>
2020-11-26 02:03:09 -0500

    first styles commit

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../images/logo-principal.jpg';
+// import { Link } from 'react-router-dom';
 
 const Home = () => (
-  <header className="head-waiter">
-    <Link to="/"><img src={logo} alt="imagen de product" className="logo-pg" /></Link>
-    <h1>Procter and Gamble </h1>
-  </header>
+  <div className="wallpaper">
+    <section className="sidebar">Temis, tu asistente legal</section>
+  </div>
 );
 export default Home;
